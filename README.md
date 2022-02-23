6bb7aa246108eb643cb224aeb6dbdddf963c544a
Sal Gerace <sgerace@gmail.com>
2022-02-23 08:48:31 -0500

    Initial prototype

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# proknow-renaming-rule-sync
+# ProKnow Renaming Rules Synchronization Tool
+
+The ProKnow Renaming Rules Synchronization Tool provides a Python script that can automatically synchronize synonym renaming rules from an Excel workbook to a ProKnow domain. This tool is focused on supporting large-scale implementations of ProKnow where managing the various permutations of synonym renaming rules can be challenging.
+
+
+## Getting Started
+
+### Installing Python
+
+Before you can run the synchronization script, you must first ensure that you have a recent version of [Python 3](https://www.python.org/downloads/) installed on your system. You can check the current version of Python by running the following command in a terminal:
+
+```
+$ python --version
+```
+
+If this command reports a version starting with "3" (e.g., "Python 3.9.5"), you have a suitable version of Python installed. Please note that in some older systems, the main `python` executable may be version "2". In that case, you may want to try to run the following command instead:
+
+```
+$ python3 --version
+```
+
+If command reports a version starting with "3" you should substitute any `python` calls in this document with `python3` instead.
+
+### Installing Python Requirements
+
+The synchronization scripts utilize several packages and modules that must be installed prior to executing the script. The easiest way to install these packages and modules in an isolated way is to utilize a [Python Virtual Environment](https://docs.python.org/3/tutorial/venv.html). Begin my running the following command from within the source directory:
+
+```
+$ python3 -m venv .venv
+```
+
+This will create a virtual environment in the `.venv` folder. Once you’ve created a virtual environment, you may activate it.
+
+On Windows, run:
+```
+.venv\Scripts\activate.bat
+```
+
+On Unix or MacOS, run:
+```
+$ source .venv/bin/activate
+```
+
+Once activated, you may install the necessary packages and modules in the virtual environment by running the following command (note that the command is run from within the virtual environment):
+
+```
+(.venv) $ pip install -r requirements.txt
+```
+
+Once installed, you can then run the script as described below.
+
+Please note that in most systems, activating a virtualenv gives you a shell function named:
+```
+$ deactivate
+```
+which you can run in order to exit the virtual environment and put things back to normal.
+
+
+## Renaming Rule Data
+
+Before running the synchronization script, you must first create the necessary Excel workbook which contain the desired synonym renaming rules. You provide the path to the workbook containing the synonym renaming rules as an argument to the script.
+
+An example Excel workbook has been provided in the `examples` directory. The format is very simple, with each column in the sheet representing a single structure. The first row represents the desired name of the structure, and all subsequent rows represent the synonyms.
+
+
+## Synchronization
+
+Once you have installed the prerequisites and created the necessary Excel workbook containing the desired synonym renaming rules, you may run the synchronization script by running the following command (substituting proper values for the url, credentials, and location of the file):
+
+```
+(.venv) $ python renaming-rules-sync.py --url https://example.proknow.com --credentials /path/to/credentials.json ./examples/renaming_rules.xlsx
+```
+
+Please note that you will be prompted each time before creating or updating renaming rules, and the script never deletes any renaming rules (however, it will list any renaming rules that were not defined in the workbook at the end of the script).
+
+You may run access the help information for the script by running the following command:
+
+```
+(.venv) $ python renaming-rules-sync.py --help
+```
---
+++ renaming-rules-sync.py
@@ -0,0 +1,219 @@
+import argparse
+import openpyxl
+import sys
+import glob
+import os
+from pathlib import Path
+from proknow import ProKnow
+from tqdm import tqdm
+
+
+########################################
+# Command Line Arguments
+
+# Define arguments
+parser = argparse.ArgumentParser(description="Synchronize ProKnow renaming rules.",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-u", "--url", required=True,
+    help="the base URL to use when making request to the ProKnow API")
+parser.add_argument("-c", "--credentials", required=True,
+    help='path to ProKnow API credentials file')
+parser.add_argument("file",
+    help="path to Excel workbook containing desired renaming rules")
+
+# Parse arguments
+args = parser.parse_args()
+
+
+########################################
+# Logging
+
+def beep(): sys.stdout.write("\a")
+
+def print_blue(skk): print("\033[94m{}\033[00m".format(skk))
+
+def print_cyan(skk): print("\033[96m{}\033[00m".format(skk))
+
+def print_green(skk): print("\033[92m{}\033[00m".format(skk))
+
+def print_magenta(skk): print("\033[95m{}\033[00m".format(skk))
+
+def print_red(skk): print("\033[91m{}\033[00m".format(skk))
+
+def print_yellow(skk): print("\033[93m{}\033[00m".format(skk))
+
+def fail(skk, msg=None):
+    print_red(skk)
+    if msg:
+        print_yellow(msg)
+    beep()
+    sys.exit(1)
+
+
+########################################
+# Utilities
+
+def is_rule_equal(a, b):
+    if a["type"] != b["type"]:
+        return False
+    elif a["value"] != b["value"]:
+        return False
+    elif len(a["criteria"]) != len(b["criteria"]):
+        return False
+    else:
+        a["criteria"].sort()
+        b["criteria"].sort()
+        for index, value in enumerate(a["criteria"]):
+            if b["criteria"][index] != value:
+                return False
+    return True
+
+def confirm(question, default="yes"):
+    valid = { "yes": True, "y": True, "ye": True, "no": False, "n": False }
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == "":
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
+
+
+
+########################################
+# ProKnow Client
+
+pk = ProKnow(args.url, credentials_file=args.credentials)
+
+
+########################################
+# Parse Rules
+
+print_magenta("Parsing Rules from Workbook...")
+
+# Load workspaces workbook and get active sheet
+wb = openpyxl.load_workbook(Path(args.file))
+ws = wb.active
+
+# Parse rules
+lookup = {}
+rules = {}
+for column in ws.iter_cols(values_only=True):
+    rule = None
+    for index, value in enumerate(column):
+        if type(value) is str and len(value) > 0:
+
+            # Ensure all values are unique
+            if value in lookup:
+                fail("Failed to read rules in '{0}' workbook".format(Path(args.file)), (
+                    "Found duplicate value '" + value + "'; already used by '" + lookup[value] + "'"
+                ))
+
+            # Create rule or add criteria
+            if index == 0:
+                if value in rules:
+                    fail("Failed to read rules in '{0}' workbook".format(Path(args.file)), (
+                        "Mutiple columns detected with desired structure name '" + value + "'"
+                    ))
+                else:
+                    rules[value] = rule = {
+                        "type": "synonyms",
+                        "criteria": [],
+                        "value": value
+                    }
+            elif value != rule["value"] and value not in rule["criteria"]:
+                rule["criteria"].append(value)
+
+    # Add unique values to lookup
+    if rule != None:
+        lookup[rule["value"]] = rule["value"]
+        for value in rule["criteria"]:
+            lookup[value] = rule["value"]
+
+
+########################################
+# Query Existing Rules
+
+print_magenta("Querying Rules from ProKnow...")
+
+# Query existing rules
+_, items = pk.requestor.get('/renaming/rules')
+desired_items = {}
+for rule in items:
+    desired_items[rule["value"]] = {
+        "rule": {
+            "id": rule["id"],
+            "type": rule["type"],
+            "criteria": rule["criteria"],
+            "value": rule["value"]
+        },
+        "state": "unknown"
+    }
+
+# Determine new and updated rules
+for value in rules:
+    rule = rules[value]
+    if value in desired_items:
+        rule["id"] = desired_items[value]["rule"]["id"]
+        if not is_rule_equal(rule, desired_items[value]["rule"]):
+            desired_items[value]["rule"] = rule
+            desired_items[value]["state"] = "updated"
+        else:
+            desired_items[value]["state"] = "unchanged"
+    else:
+        desired_items[value] = {
+            "rule": rule,
+            "state": "created"
+        }
+
+
+########################################
+# Determine Desired Rules
+
+print_magenta("Synchronizing Renaming Rules...")
+
+# Construct body and identify state of rules
+body = []
+created = 0
+updated = 0
+unknown = []
+for value in desired_items:
+    item = desired_items[value]
+    body.append(item["rule"])
+    if item["state"] == "created":
+        created += 1
+    elif item["state"] == "updated":
+        updated += 1
+    elif item["state"] == "unknown":
+        unknown.append(item["rule"])
+
+# Confirm creation and updating of rules
+if created > 0 or updated > 0:
+    print_yellow(" Rules have changed ({0} created, {1} updated)".format(created, updated))
+    if not confirm("Are you sure you wish to synchronize renaming rules?"):
+        fail("Synchronization aborted")
+    _, res = pk.requestor.put('/renaming/rules', json=body)
+    print_green(" Renaming rules successfully synchronized")
+else:
+    print_green(" All rules exist and are up to date")
+
+
+########################################
+# Reporting
+
+if len(unknown) > 0:
+    print_magenta("Identifying Unknown Renaming Rules...")
+    if len(unknown) > 0:
+        print_yellow(" Found {0} renaming rule(s) not defined in workbook:".format(len(unknown)))
+        for item in unknown:
+            print("  rename to '{0}' ({1})".format(item["value"], item["type"]))
